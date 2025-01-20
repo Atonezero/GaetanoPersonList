43d73fa59c5ffae9bc1bfcb8eeb63bf1e9e4e859
Atonezero <146082513+Atonezero@users.noreply.github.com>
2025-01-20 09:31:21 +0000

    nothing important

--- ElencoVeicoliJava/FactoryVeicoli.java
+++ ElencoVeicoliJava/FactoryVeicoli.java
@@ -3,14 +3,14 @@ import java.util.Map;
 import java.util.function.Supplier;
 
     public class FactoryVeicoli {
-        public static Veicolo creaVeicolo(int tipo, String marca, String modello, String targa, int numeroPorte, boolean haBauletto) {
+        public static Veicolo creaVeicolo(int tipo, String marca, String modello, String targa, int numeroPorte, Boolean haBauletto, String pesoMassimo) {
             switch (tipo) {
                 case 1: 
                     return new Automobili(marca, modello, targa, numeroPorte);
                 case 2: 
                     return new Motocicli(marca, modello, targa, haBauletto);
                 case 3:
-                    return new Mezzi_Pesanti(marca, modello, targa, pesoMassimo)
+                    return new Mezzi_Pesanti(marca, modello, targa, pesoMassimo);
                 default:
                     throw new IllegalArgumentException("Tipo di veicolo non valido.");
             }
